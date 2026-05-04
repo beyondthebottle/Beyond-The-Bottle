fccbd4ea1351de1968bbaa40116ccb8545dc8ee6
beyondthebottle <blackandromeda.t@gmail.com>
2026-05-04 16:34:12 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+let reseñas = [];
+
+function guardarEnMemoria() {
+  localStorage.setItem("resenas", JSON.stringify(reseñas));
+}
+
+function cargarResenas() {
+  let datosGuardados = localStorage.getItem("resenas");
+  
+  if (datosGuardados) {
+    reseñas = JSON.parse(datosGuardados);
+  }
+  
+  mostrarResenas();
+}
+
+function mostrarResenas() {
+  let contenedor = document.getElementById("contenedorResenas");
+  contenedor.innerHTML = "";
+  
+  reseñas.forEach(function(resena, index) {
+    let nuevaResena = document.createElement("div");
+    nuevaResena.className = "card";
+    
+    nuevaResena.innerHTML = `
+            <p><strong>${resena.nombre}:</strong> ${resena.comentario}</p>
+            <button onclick="darLike(${index})">❤️ ${resena.likes}</button>
+        `;
+    
+    contenedor.appendChild(nuevaResena);
+  });
+}
+
+function agregarResena() {
+  let nombre = document.getElementById("nombre").value;
+  let comentario = document.getElementById("comentario").value;
+  
+  if (nombre === "" || comentario === "") {
+    alert("Por favor completa todos los campos");
+    return;
+  }
+  
+  let nueva = {
+    nombre: nombre,
+    comentario: comentario,
+    likes: 0
+  };
+  
+  reseñas.push(nueva);
+  guardarEnMemoria();
+  mostrarResenas();
+  
+  document.getElementById("comentario").value = "";
+}
+
+function darLike(index) {
+  reseñas[index].likes++;
+  guardarEnMemoria();
+  mostrarResenas();
+}
+
+function guardarNombre() {
+  let nombre = document.getElementById("nombre").value;
+  
+  if (nombre === "") {
+    alert("Escribe tu nombre");
+    return;
+  }
+  
+  localStorage.setItem("nombreUsuario", nombre);
+  alert("Nombre guardado 👌");
+}
+
+function cargarNombre() {
+  let nombreGuardado = localStorage.getItem("nombreUsuario");
+  
+  if (nombreGuardado) {
+    document.getElementById("nombre").value = nombreGuardado;
+  }
+}
+
+cargarResenas();
+cargarNombre();
